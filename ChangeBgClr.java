44fc8d24ef1a97575104bad8f387164feacaef70
Sowjanya6 <sowjanya.s.murthh@gmail.com>
2016-05-14 17:41:58 +0530

    Update ChangeBgClr.java

--- ChangeBgClr.java
+++ ChangeBgClr.java
@@ -2,12 +2,16 @@
 	    //private static Shape decoratorShirt;
 		public ChangeBgClr(Shape decoratorShirt) {
 			super(decoratorShirt);
+			Scanner in = new Scanner(System.in);
+		       System.out.println("Enter the background color to be printed on ur shirt");
+                        String clr = in.nextLine();
+			System.out.println("the text on the tshirt is "+clr);		
 	        //System.out.println("Adding color");
 		}
 		
 		public String getShirt(){
 
-			return decoratedShirt.getShirt() +"clr changed to pink,  ";
+			return decoratedShirt.getShirt();
 		}
 	
    	   public double getCost(){
