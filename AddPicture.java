b9dbb78963e464e0c04d57d4791981503a5db957
Sowjanya6 <sowjanya.s.murthh@gmail.com>
2016-05-14 17:38:47 +0530

    Update AddPicture.java

--- AddPicture.java
+++ AddPicture.java
@@ -3,10 +3,14 @@ public class AddPicture extends ShirtDecorator{
 		//private static Shape decoratorShirt;
 		public AddPicture(Shape decoratorShirt) {
 			super(decoratorShirt);
+			Scanner in = new Scanner(System.in);
+		        System.out.println("Enter the picture to be printed on ur shirt");
+                        String pic = in.nextLine();
+			System.out.println("the text on the tshirt is"+pic);		
 			// System.out.println("Adding picture");
 		}
 		public String getShirt(){
-			return decoratedShirt.getShirt() +"picture is added, ";
+			return decoratedShirt.getShirt();
 		}
 		   public double getCost(){
 			   System.out.println("Cost for printed Tshirt is"+ 100.0);
